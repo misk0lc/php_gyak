662a35aa0edd78a86e9d8086c77df355bf023a47
misk0lc <gyorfi1552@gmail.com>
2025-09-04 10:46:55 +0200

    előkészítés

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php
+
+    /*
+    Szükséges programok: XAMPP (Apache, MYSQL) , Git, github repo, VSC
+    <?php ... ?>
+
+    Változók
+    */
+    $name = "misk0lc";
+    $age = "18";
+    $city = "Chicago";
+    var_dump($name);
+
+    echo "<br>Név: $name Életkor: $age Lakóhely: $city";
+?>
