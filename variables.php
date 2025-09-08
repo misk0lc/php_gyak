99c6f676711187eea9cd606508a19d83480d5d4a
misk0lc <gyorfi1552@gmail.com>
2025-09-08 08:12:41 +0200

    git clone

--- variables.php
+++ variables.php
@@ -3,13 +3,39 @@
     /*
     Szükséges programok: XAMPP (Apache, MYSQL) , Git, github repo, VSC
     <?php ... ?>
-
-    Változók
     */
+
+    //Változók
     $name = "misk0lc";
     $age = "18";
     $city = "Chicago";
     var_dump($name);
 
     echo "<br>Név: $name Életkor: $age Lakóhely: $city";
+    //Konstans
+    define("PI", 3.14159265359);
+    echo "<br>", PI,"<br>";
+
+    //Adattípusok: string, int, float, bool, array
+
+    $message = "egy";
+    echo "Kiír ", $message, " értéket ", "<br>\n";
+    echo "Kiír  $message értéket. <br>\n";
+    echo "Kiír {$message}darab értéket. <br>\n";
+    echo 'Kiír {$message}darab értéket. <br>\n';
+
+    //print "Kiír ", $message, " értéket ", "<br>\n";
+    print "Kiír  $message értéket. <br>\n";
+    print 'Kiír {$message}darab értéket. <br>\n';
+
+    /*
+    git parancsok
+        git init:
+        git add . :
+        git commit -m "message":
+        git remote add origin https://github.com/felhnev/reponeve.git
+            git config --global user.name
+            git config --global user.email
+        git branch -m main :
+    */
 ?>
