75405b59b85cfbc754ec6fd18965a59b8f24a5da
misk0lc <gyorfi1552@gmail.com>
2025-09-15 08:04:43 +0200

    morzsa egy buzi

---
+++ get_post.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Űrlapkezelés</h1>
+    <form action="" method="post">
+        <label for="name">Név:</label>
+        <input type="text" name="name" id="name">
+        <button type="submit">Küld</button>
+    </form>
+</body>
+</html>
+<!--1db mező kerem a neved, mögötte submit gomb-->
